416c1926f3f7254881b1b88470094240cd3d8389
Domdar7 <dmnkpv7@gmail.com>
2022-12-14 11:47:07 +0100

    promjena
    promjena

--- Assets/Scripts/DestroyPlayerMech.cs
+++ Assets/Scripts/DestroyPlayerMech.cs
@@ -5,18 +5,18 @@ public class DestroyPlayerMech : MonoBehaviour
     [SerializeField] float stopDistance;
     [SerializeField] float timeToKill;
     public Vector2 playerObject;
-    public Rigidbody2D thisRb;
+    public Vector2 thisRb;
     private void Start()
     {
         playerObject = GameObject.FindGameObjectWithTag("ProtectPlayer").transform.position;
-        thisRb = GetComponent<Rigidbody2D>();
+        //thisRb = GetComponent<Rigidbody2D>();
     }
     private void Update()
     {
 
        
         //thisRb.position = Vector2.MoveTowards(transform.position, playerObject, stopDistance)*Time.deltaTime * timeToKill;
-        thisRb.position = Vector2.Lerp(transform.position, playerObject, timeToKill * Time.time);
+        thisRb = Vector2.Lerp(transform.position, playerObject, timeToKill * Time.time);
     }
     private void OnMouseDown()
     {
