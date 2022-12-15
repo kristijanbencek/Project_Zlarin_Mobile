801f687b421d7693d8dbd28a2974ced83242336a
kristijan_bencek <58005221+kristijanbencek@users.noreply.github.com>
2022-12-15 13:19:04 +0100

    save

---
+++ Assets/Scripts/Kristijan/SaveSystem/SaveSystem.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem 
+{
+  public static void Save(GameData data)
+    {
+        string path = Application.persistentDataPath + "/data.qnd";
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream fs = new FileStream(path, FileMode.Create);
+        formatter.Serialize(fs, data);
+        fs.Close();
+    }
+
+    public static GameData Load()
+    {
+        if (!File.Exists(GetPath()))
+        {
+            GameData emptyData = new GameData();
+            Save(emptyData);
+            return emptyData;
+        }
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream fs = new FileStream(GetPath(), FileMode.Open);
+        GameData data = formatter.Deserialize(fs) as GameData;
+        fs.Close();
+
+        return data;
+    }
+
+
+    private static string GetPath()
+    {
+        return Application.persistentDataPath + "/data.qnd";
+    }
+
+}
---
+++ Assets/Scripts/Kristijan/SaveSystem/Test.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Test : MonoBehaviour
+{
+    public GameData gameData;
+
+
+    public int health = 1;
+    public TMP_Text healthTXT;
+
+    private void Awake()
+    {
+        gameData = SaveSystem.Load();
+    }
+
+
+
+    void Update()
+    {
+        healthTXT.text = gameData.totalHealth.ToString();
+    }
+
+    public void AddHealth()
+    {                
+        gameData.totalHealth += health;
+        SaveSystem.Save(gameData);
+    }
+}
